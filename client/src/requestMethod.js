47778ac67e3e202c797305bed1744181e8c9af52
Damjan Savkovic <damjans92@gmail.com>
2022-06-09 13:37:31 +0200

    token error

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -1,23 +1,28 @@
 import axios from "axios";
 
 const BASE_URL = "https://clothingshop-ds.herokuapp.com/api";
-let token = "";
-function TOKEN() {
-  if (
-    JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
-      .currentUser?.accessToken
-  ) {
-    token = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-      .currentUser?.accessToken;
-  } else {
-    token = "";
-  }
-}
-TOKEN();
+// let token = "";
+// function TOKEN() {
+//   if (
+//     JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+//       .currentUser?.accessToken
+//   ) {
+//     token =
+//       JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+//         .currentUser?.accessToken || "";
+//   } else {
+//     token = "";
+//   }
+// }
+// TOKEN();
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
+// export const userRequest = axios.create({
+//   baseURL: BASE_URL,
+//   headers: { token: `Bearer ${token}` },
+// });
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { token: `Bearer ${token}` },
+  headers: { token: `Bearer ` },
 });
