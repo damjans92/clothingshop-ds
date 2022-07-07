b10857780231b0b71067ffd9cb70a55de4f98d97
Damjan Savkovic <damjans92@gmail.com>
2022-07-07 19:04:01 +0200

    removed comments navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import SearchIcon from "@mui/icons-material/Search";
 import Badge from "@mui/material/Badge";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { mobile, mobilePt } from "../responsive";
 import { Link } from "react-router-dom";
 import { logout } from "../redux/userRedux";
-import { publicRequest } from "../requestMethod";
 import Search from "./Search";
 
 const Container = styled.div`
@@ -34,24 +32,6 @@ const Language = styled.span`
   cursor: pointer;
   ${mobilePt({ display: "none" })}
 `;
-
-// const SearchContainer = styled.div`
-//   border: 0.5px solid lightgray;
-//   display: flex;
-//   align-items: center;
-//   margin-left: 25px;
-//   padding: 5px;
-//   position: relative;
-//   ${mobilePt({ margin: "0 15px", width: "100%" })}
-// `;
-
-// const Input = styled.input`
-//   border: none;
-//   ${mobilePt({ width: "100%" })}
-//   &:focus {
-//     outline: none;
-//   }
-// `;
 
 const Center = styled.div`
   flex: 1;
@@ -93,93 +73,22 @@ const LinksStyled = styled(Link)`
     text-decoration: ${(props) => (props.underline ? "underline" : "none")};
   }
 `;
-// const SearchDropdown = styled.div`
-//   position: absolute;
-//   top: 30px;
-//   left: 0;
-//   width: 100%;
-//   z-index: 2;
-//   background-color: #fff;
-//   border: 1px solid #ccc;
-//   padding: 10px;
-// `;
-// const SearchItemTitle = styled.h4`
-//   margin: 5px 0;
-//   font-size: 16px;
-// `;
+
 const Navbar = () => {
   const quantity = useSelector((state) => state.cart.quantity);
   const cUser = useSelector((state) => state.user.currentUser);
-  // const [searchValue, setSearchValue] = useState("");
-  // const [searchProducts, setSearchProducts] = useState([]);
-  // const [searchOpen, setSearchOpen] = useState(false);
-  const dispatch = useDispatch();
 
-  // const searchRef = useRef();
+  const dispatch = useDispatch();
 
   const handleLogout = () => {
     dispatch(logout());
   };
-
-  // useEffect(() => {
-  //   const searchHandler = async (e) => {
-  //     const res = await publicRequest.post("/products/search", {
-  //       searchQuery: searchValue,
-  //     });
-
-  //     setSearchProducts(res.data);
-  //   };
-  //   if (searchValue !== "") {
-  //     searchHandler();
-  //   } else {
-  //     setSearchProducts([]);
-  //   }
-
-  //   return () => setSearchProducts([]);
-  // }, [searchValue]);
-
-  // useEffect(() => {
-  //   const handleOutsideClick = (e) => {
-  //     if (
-  //       searchOpen &&
-  //       searchRef.current &&
-  //       !searchRef.current.contains(e.target)
-  //     ) {
-  //       setSearchOpen(false);
-  //     }
-  //   };
-
-  //   document.addEventListener("mousedown", handleOutsideClick);
-
-  //   return () => {
-  //     document.removeEventListener("mousedown", handleOutsideClick);
-  //   };
-  // }, [searchOpen]);
 
   return (
     <Container>
       <Wrapper>
         <Left>
           <Search />
-          {/* <SearchContainer ref={searchRef}>
-            <Input
-              placeholder="search"
-              onChange={(e) => setSearchValue(e.target.value)}
-              onClick={() => setSearchOpen(true)}
-              value={searchValue}
-            />
-
-            <SearchIcon style={{ color: "grey", fontSize: 16 }} />
-            {searchOpen && searchProducts.length > 0 && (
-              <SearchDropdown>
-                {searchProducts.map((p) => (
-                  <LinksStyled to={`/product/${p._id}`} underline>
-                    <SearchItemTitle>{p.title}</SearchItemTitle>
-                  </LinksStyled>
-                ))}
-              </SearchDropdown>
-            )}
-          </SearchContainer> */}
         </Left>
         <Center>
           <Logo>
